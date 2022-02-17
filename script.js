6a22566c34fcac356e7cb5aec40233c361f47d28
Ivanna Contreras <ivanna.aws@gmail.com>
2022-02-16 18:27:12 -0600

    updates

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('this is the script file')
+
+if(navigator.geolocation) {
+    console.log("Location is available")
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords);
+      });
+  } else {
+    console.log("Location not available")
+  }
+
