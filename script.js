76fb55b0f7a479674faf23f918290a5ecf8ea82f
Ivanna Contreras <ivanna.aws@gmail.com>
2022-02-17 17:34:27 -0600

    api

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ if(navigator.geolocation) {
     console.log("Location not available")
   }
 
+
+// const AWS = require("aws-sdk");
+// const credentials = new AWS.CognitoIdentityCredentials({
+//  IdentityPoolId: "us-east-2:d9bd6c65-e4f6-4a35-b007-d1e5592c6ec5" // e.g., us-east-1:d40a3a95-a6b5-4816-a15ca15ac4f0930d
+// });
+// const client = new AWS.Location({
+//  credentials,
+//  region: AWS.config.region || "<region>"
+// });
+// console.log(await client.getMapStyleDescriptor("ExampleMap").promise());
