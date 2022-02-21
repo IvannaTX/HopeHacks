1712cb15c1d22b10225c185aaf939cf5f4d0a030
Balexm95 <bria.mccoy95@gmail.com>
2022-02-21 04:13:12 -0500

    make it better

--- JS/server.js
+++ JS/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const routes = require("./routes")
 const port = 5000;
 //use static files like CSS
 app.use(express.static('public'));
-
-
+app.use('/',routes)
+app.set("port",port)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
