51d4944a896e765a5e6b3bc04eac62b2aca10a96
Balexm95 <bria.mccoy95@gmail.com>
2022-02-22 21:03:50 -0500

    updated contact

---
+++ AWS_attempt/index.js
@@ -0,0 +1,116 @@
+ // This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initAutocomplete() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -33.8688, lng: 151.2195 },
+      zoom: 13,
+      mapTypeId: "roadmap",
+    });
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+  
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
+  
+    let markers = [];
+  
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+  
+      if (places.length == 0) {
+        return;
+      }
+  
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+  
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+  
+      places.forEach((place) => {
+        if (!place.geometry || !place.geometry.location) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+  
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+  
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            icon,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
+  
+  // The following example creates five accessible and
+  // focusable markers.
+  function initMap() {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: { lat: 34.84555, lng: -111.8035 },
+      });
+      const input = document.getElementById("pac-input");
+      const searchBox = new google.maps.places.SearchBox(input);
+      // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+      // receives the initial focus when tab is pressed. Use arrow keys to
+      // move between markers; press tab again to cycle through the map controls.
+      const tourStops = [
+        [{ lat: 34.8791806, lng: -111.8265049 }, "Boynton Pass"],
+        [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
+        [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
+        [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
+        [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
+      ];
+      // Create an info window to share between markers.
+      const infoWindow = new google.maps.InfoWindow();
+    
+      // Create the markers.
+      tourStops.forEach(([position, title], i) => {
+        const marker = new google.maps.Marker({
+          position,
+          map,
+          title: `${i + 1}. ${title}`,
+          label: `${i + 1}`,
+          optimized: false,
+        });
+    
+        // Add a click listener for each marker, and set up the info window.
+        marker.addListener("click", () => {
+          infoWindow.close();
+          infoWindow.setContent(marker.getTitle());
+          infoWindow.open(marker.getMap(), marker);
+        });
+      });
+    }
